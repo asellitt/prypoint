33f10b6d9ea3fb5595d0be8ff53e87fa53c168d1
Anthony Sellitti <anthony.sellitti@envato.com>
2013-10-09 15:10:31 +1100

    use better_errors

--- prypoint.rb
+++ prypoint.rb
@@ -1,45 +1,64 @@
 require 'sinatra'
 require 'pry-debugger'
+require 'better_errors'
+
+configure do
+  use BetterErrors::Middleware
+end
 
 get '/json*' do
-  response.headers['Content-Type'] = 'application/javascript';
-  response.headers['Access-Control-Allow-Origin'] = '*';
-  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  f_u_cors
+  response.headers['Content-Type'] = 'application/javascript'
   request.params["json"]
 end
 
 post '/json*' do
-  response.headers['Content-Type'] = 'application/javascript';
-  response.headers['Access-Control-Allow-Origin'] = '*';
-  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  f_u_cors
+  response.headers['Content-Type'] = 'application/javascript'
   request.params["json"]
 end
 
 options '/json*' do
-  response.headers['Content-Type'] = 'application/javascript';
-  response.headers['Access-Control-Allow-Origin'] = '*';
-  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  f_u_cors
+  response.headers['Content-Type'] = 'application/javascript'
   request.params["json"]
 end
 
 get '/pry*' do
-  response.headers['Access-Control-Allow-Origin'] = '*';
-  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  f_u_cors
   binding.pry
 end
 
 post '/pry*' do
-  response.headers['Access-Control-Allow-Origin'] = '*';
-  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  f_u_cors
   pry.binding
 end
 
 options '/pry*' do
-  response.headers['Access-Control-Allow-Origin'] = '*';
-  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  f_u_cors
   pry.binding
+end
+
+get '/error*' do
+  f_u_cors
+  raise
+end
+
+post '/error*' do
+  f_u_cors
+  raise
+end
+
+options '/error*' do
+  f_u_cors
+  raise
 end
 
 get '/' do
   "Oh Hai! You know what to do..."
+end
+
+def f_u_cors
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
 end
