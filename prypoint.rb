5c684b46bc65c5e484c09430a6b8edd55289d4d4
Anthony Sellitti <anthony.sellitti@envato.com>
2013-10-09 14:40:23 +1100

    herokize (step 1)

--- prypoint.rb
+++ prypoint.rb
@@ -1,18 +1,41 @@
 require 'sinatra'
 require 'pry-debugger'
 
-get '/json/*' do
+get '/json*' do
+  response.headers['Content-Type'] = 'application/javascript';
+  response.headers['Access-Control-Allow-Origin'] = '*';
+  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
   request.params["json"]
 end
 
-post '/json/*' do
+post '/json*' do
+  response.headers['Content-Type'] = 'application/javascript';
+  response.headers['Access-Control-Allow-Origin'] = '*';
+  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  request.params["json"]
+end
+
+options '/json*' do
+  response.headers['Content-Type'] = 'application/javascript';
+  response.headers['Access-Control-Allow-Origin'] = '*';
+  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
   request.params["json"]
 end
 
 get '/*' do
+  response.headers['Access-Control-Allow-Origin'] = '*';
+  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
   binding.pry
 end
 
 post '/*' do
+  response.headers['Access-Control-Allow-Origin'] = '*';
+  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+  pry.binding
+end
+
+options '/*' do
+  response.headers['Access-Control-Allow-Origin'] = '*';
+  response.headers['Access-Control-Allow-Headers'] = request.env['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
   pry.binding
 end
